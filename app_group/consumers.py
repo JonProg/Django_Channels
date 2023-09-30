8e51c7c923969c0afb6ac9f87ce09bd980592857
jon_prog <jonashenri.aquino@gmail.com>
2023-09-30 15:28:01 -0300

    modified asgi.py

--- app_group/consumers.py
+++ app_group/consumers.py
@@ -1,6 +1,5 @@
 import json
 
-from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
--- project/asgi.py
+++ project/asgi.py
@@ -13,16 +13,18 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from app_group.routings import websocket_urlpatterns
+from app_group import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-      URLRouter(
-          AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns))
-      )
-  ),
-})
+django_asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+  {
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    )
+  }
+)
 
