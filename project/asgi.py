9733d9418370ccd91c57240061cc89d9d82b6800
jon_prog <jonashenri.aquino@gmail.com>
2023-09-30 14:56:02 -0300

    modified consumers

--- project/asgi.py
+++ project/asgi.py
@@ -8,14 +8,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+
 from django.core.asgi import get_asgi_application
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from app_group.routings import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
-  # Just HTTP for now. (We can add other protocols later.)
+  "websocket": AuthMiddlewareStack(
+      URLRouter(
+          AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns))
+      )
+  ),
 })
 
