0297cd62839ed4206933fad85926cce37562ba55
jon_prog <jonashenri.aquino@gmail.com>
2023-10-01 09:19:03 -0300

    modified apps.py

--- app_group/apps.py
+++ app_group/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AppGroupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_group'
+
+def ready(self):
+    import app_group.signals
