8b9022ebc53a9ca3a99ff1f18b6c8f5b433101dd
jon_prog <jonashenri.aquino@gmail.com>
2023-10-01 12:46:24 -0300

    modified admin.py

--- app_group/admin.py
+++ app_group/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from models import Groups
+from .models import Groups
 
 @admin.register(Groups)
 class GroupsAdmin(admin.ModelAdmin):
-    list_display = 'id', 'name', 'slug',
-    list_display_links = 'name',
-    search_fields = 'id', 'name', 'slug',
+    list_display = 'id', 'group', 'slug',
+    list_display_links = 'group',
+    search_fields = 'id', 'group', 'slug',
     list_per_page = 10
     ordering = '-id',
     prepopulated_fields = {
-        "slug": ('name',),
+        "slug": ('group',),
     }
