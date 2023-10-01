20f4d4e681ea8fb8aed91805b2a2079073c78721
jon_prog <jonashenri.aquino@gmail.com>
2023-10-01 13:13:54 -0300

    modified urls.py

--- app_group/urls.py
+++ app_group/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from app_group import views
 
+app_name = 'app_group'
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("chat/<slug:root>", name="group")
 ]
